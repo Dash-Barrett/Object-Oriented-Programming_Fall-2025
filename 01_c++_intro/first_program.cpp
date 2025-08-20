c7acf01e4594ef66ac31e85e723719e2d3371783
Dash Barrett <dbarrett7614@floridapoly.edu>
2025-08-20 18:39:06 +0000

    The Introduction

---
+++ 01_c++_intro/first_program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+using std::cout;
+
+// Create a main function
+int main(void) {
+    std::cout << "Hello COP3337!" << std:: endl;
+
+    // std - standard namespace
+    // :: - scope resolution operator
+    // cout - console output (object of an iostream class)
+    // << - stream insertion operator
+    // endl - end of line
+
+    return 0;
+}
